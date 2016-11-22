7241393ec9e093725f148771385b4279c81ce382
Adam Shinn <adam.shinn@kwhanalytics.com>
2016-11-21 16:51:07 -0800

    Add versioneer for compatibility.

---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    raise RuntimeError('setuptools is required')
+
+
+import versioneer
+
+
+DESCRIPTION = ('Functions for analytics the degradation rate of photovoltaic'
+               ' systems.')
+LONG_DESCRIPTION = """
+Rdtools is a collection of functions that help analyze.
+
+Source code: https://github.com/kwhanalytics/rdtools
+"""
+
+DISTNAME = 'rdtools'
+LICENSE = 'MIT'
+AUTHOR = 'Rdtools Python Developers'
+MAINTAINER_EMAIL = 'adam.b.shinn@gmail.com'
+URL = 'https://github.com/kwhanalytics/rdtools'
+
+INSTALL_REQUIRES = ['numpy >= 1.9.0',
+                    'pandas >= 0.14.0',
+                    'pvlib >= 0.4.1',
+                    'pytz',
+                    'six']
+TESTS_REQUIRE = ['pytest', 'nose']
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Beta',
+    'License :: MIT License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Scientific/Engineering',
+]
+
+setuptools_kwargs = {
+    'zip_safe': False,
+    'scripts': [],
+    'include_package_data': True
+}
+
+# set up pvlib packages to be installed and extensions to be compiled
+PACKAGES = ['rdtools']
+
+setup(name=DISTNAME,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      packages=PACKAGES,
+      install_requires=INSTALL_REQUIRES,
+      tests_require=TESTS_REQUIRE,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      classifiers=CLASSIFIERS,
+      **setuptools_kwargs)
