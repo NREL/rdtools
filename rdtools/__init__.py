a8939ab7eb626327d3b12a0365a61fb238bc0974
Michael Deceglie <michael.deceglie@nrel.gov>
2017-02-21 15:33:44 -0700

    add new degradation functions to __init__.py

--- rdtools/__init__.py
+++ rdtools/__init__.py
@@ -1,5 +1,7 @@
 from energy_normalization import normalize_with_sapm
 from degradation import degradation_ols
+from degradaion import degradation_classical_decomposition
+from degradaion import degradation_year_on_year
 
 from ._version import get_versions
 __version__ = get_versions()['version']
