46f93d71601b9a1a536d7471b56939dc0b66e9e7
Michael Deceglie <mdecegli@mdecegli-30388s.nrel.gov>
2017-04-25 16:03:21 -0600

    Updated to be PEP8 compliant

--- rdtools/degradation.py
+++ rdtools/degradation.py
@@ -32,36 +32,35 @@ def degradation_ols(normalized_energy):
 
     normalized_energy.name = 'normalized_energy'
     df = normalized_energy.to_frame()
-    
-    #calculate a years column as x value for regression, ignoreing leap years
+
+    # calculate a years column as x value for regression, ignoreing leap years
     day_diffs = (df.index - df.index[0])
-    df['days'] = day_diffs.astype('timedelta64[s]')/(60*60*24)
-    df['years'] = df.days/365.0
-    
-    #add intercept-constant to the exogeneous variable
+    df['days'] = day_diffs.astype('timedelta64[s]') / (60 * 60 * 24)
+    df['years'] = df.days / 365.0
+
+    # add intercept-constant to the exogeneous variable
     df = sm.add_constant(df)
-    
-    #perform regression
-    ols_model = sm.OLS(endog = df.normalized_energy, exog = df.loc[:,['const','years']],
-                       hasconst = True, missing = 'drop' )
-    
+
+    # perform regression
+    ols_model = sm.OLS(endog=df.normalized_energy, exog=df.loc[:, ['const', 'years']],
+                       hasconst=True, missing='drop')
+
     results = ols_model.fit()
-    
+
     # collect intercept and slope
     b, m = results.params
-    
+
     # rate of degradation in terms of percent/year
     Rd_pct = 100.0 * m / b
-    
-    #Calculate RMSE
+
+    # Calculate RMSE
     rmse = np.sqrt(results.mse_resid)
-    
-    #Collect standrd errors
+
+    # Collect standrd errors
     stderr_b, stderr_m = results.bse
 
-    #Monte Carlo for error in degradation rate
+    # Monte Carlo for error in degradation rate
     Rd_CI = _degradation_CI(results)
-
 
     degradation = {
         'Rd_pct': Rd_pct,
@@ -76,7 +75,7 @@ def degradation_ols(normalized_energy):
 
     return degradation
 
-      
+
 def degradation_classical_decomposition(normalized_energy):
     '''
     Description
@@ -100,67 +99,67 @@ def degradation_classical_decomposition(normalized_energy):
         pandas series for the annual rolling mean ('series'),
         Mann-Kendall test trend ('mk_test_trend')
     '''
-    
+
     normalized_energy.name = 'normalized_energy'
     df = normalized_energy.to_frame()
-    
+
     df_check_freq = df.copy()
 
-    #The frequency attribute will be set to None if rows are dropped.
-    #We can use this to check for missing data and raise a ValueError.
+    # The frequency attribute will be set to None if rows are dropped.
+    # We can use this to check for missing data and raise a ValueError.
     df_check_freq = df_check_freq.dropna()
 
     if df_check_freq.index.freq is None:
         raise ValueError('Classical decomposition requires a regular time series with'
                          ' defined frequency and no missing data.')
 
-    #calculate a years column as x value for regression, ignoreing leap years
+    # calculate a years column as x value for regression, ignoreing leap years
     day_diffs = (df.index - df.index[0])
-    df['days'] = day_diffs.astype('timedelta64[s]')/(60*60*24)
-    df['years'] = df.days/365.0
-    
-    #Compute yearly rolling mean to isolate trend component using moving average
+    df['days'] = day_diffs.astype('timedelta64[s]') / (60 * 60 * 24)
+    df['years'] = df.days / 365.0
+
+    # Compute yearly rolling mean to isolate trend component using moving average
     it = df.iterrows()
     energy_ma = []
     for i, row in it:
-        if row.years-0.5 >= min(df.years) and row.years+0.5 <= max(df.years):
-            roll = df[(df.years <= row.years+0.5) & (df.years >= row.years-0.5)]
+        if row.years - 0.5 >= min(df.years) and row.years + 0.5 <= max(df.years):
+            roll = df[(df.years <= row.years + 0.5) & (df.years >= row.years - 0.5)]
             energy_ma.append(roll.normalized_energy.mean())
         else:
             energy_ma.append(np.nan)
-    
+
     df['energy_ma'] = energy_ma
-    
-    #add intercept-constant to the exogeneous variable
+
+    # add intercept-constant to the exogeneous variable
     df = sm.add_constant(df)
-    
-    #perform regression
-    ols_model = sm.OLS(endog = df.energy_ma, exog = df.loc[:,['const','years']],
-                       hasconst = True, missing = 'drop' )
-    
+
+    # perform regression
+    ols_model = sm.OLS(endog=df.energy_ma, exog=df.loc[:, ['const', 'years']],
+                       hasconst=True, missing='drop')
+
     results = ols_model.fit()
-    
+
     # collect intercept and slope
     b, m = results.params
-    
+
     # rate of degradation in terms of percent/year
     Rd_pct = 100.0 * m / b
-    
-    #Calculate RMSE
+
+    # Calculate RMSE
     rmse = np.sqrt(results.mse_resid)
-    
-    #Collect standrd errors
+
+    # Collect standrd errors
     stderr_b, stderr_m = results.bse
 
-    #Perform Mann-Kendall 
+    # Perform Mann-Kendall
     test_trend, h, p, z = _mk_test(df.energy_ma.dropna(), alpha=0.05)
 
-    #Monte Carlo for error in degradation rate
+    # Monte Carlo for error in degradation rate
     Rd_CI = _degradation_CI(results)
 
     degradation = {
         'Rd_pct': Rd_pct,
-        'Rd_CI':Rd_CI,
+        'Rd_CI': Rd_CI,
         'slope': m,
         'intercept': b,
         'rmse': rmse,
@@ -172,10 +171,9 @@ def degradation_classical_decomposition(normalized_energy):
     }
 
     return degradation
-     
 
 
-def degradation_year_on_year(normalized_energy, freq = 'D'):
+def degradation_year_on_year(normalized_energy, freq='D'):
     '''
     Description
     -----------
@@ -184,18 +182,18 @@ def degradation_year_on_year(normalized_energy, freq = 'D'):
     Parameters
     ----------
     normalized_energy: Pandas data series (numeric) containing corrected performance ratio
-        timeseries index in monthly format              
+        timeseries index in monthly format
     freq: string to specify aggregation frequency, default value 'D' (daily)
-    
+
     Returns
     -------
     degradation_values: dictionary
         Contains values for median degradation rate and standard error
         'Rd_med', 'Rd_stderr_pct', 'YoY_filtered'
         where YoY_filtered is list containing year on year data for
-	specified frequency
+        specified frequency
     '''
-    
+
     if freq == 'MS':
         # monthly (month start)
         normalized_energy = normalized_energy.resample('MS').mean()
@@ -221,14 +219,14 @@ def degradation_year_on_year(normalized_energy, freq = 'D'):
 
     # year-on-year approach
     YoYresult = normalized_energy.diff(YearSampleSize) / normalized_energy * 100
-    
-    def remove_outliers(x): 
+
+    def remove_outliers(x):
         '''
         Description
         -----------
         Remove data points greater or smaller than 100: the system can only lose 100%/year,
         however arbitrary large number can originate by division close to zero!
-   
+
         Parameters
         ----------
         x: float, element of list
@@ -238,12 +236,12 @@ def degradation_year_on_year(normalized_energy, freq = 'D'):
         x: float x if absolute value of x is < 100
         '''
         if x < 100 and x > -100:
-            return x    
-        
+            return x
+
     YoY_filtered1 = filter(remove_outliers, YoYresult)
-  
-    med1 = np.median(YoY_filtered1)                       
-    
+
+    med1 = np.median(YoY_filtered1)
+
     # bootstrap to determine 68% CI for the 2 different outlier removal methods
     n1 = len(YoY_filtered1)
     reps = 1000
@@ -252,17 +250,17 @@ def degradation_year_on_year(normalized_energy, freq = 'D'):
     mb1.sort()
     lpc1 = np.percentile(mb1, 15.9)
     upc1 = np.percentile(mb1, 84.1)
-    unc1 = np.round(upc1 - lpc1,2)
-    
+    unc1 = np.round(upc1 - lpc1, 2)
+
     degradation_values = {
         'Rd_median': med1,
         'Rd_stderr_pct': unc1,
-        'YoY_filtered':YoY_filtered1
-        }
+        'YoY_filtered': YoY_filtered1
+    }
     return degradation_values
-      
-    
-def _mk_test(x, alpha = 0.05):  
+
+
+def _mk_test(x, alpha=0.05):
     '''
     Description
     -----------
@@ -278,11 +276,11 @@ def _mk_test(x, alpha = 0.05):
     trend: string, tells the trend (increasing, decreasing or no trend)
     h: boolean, True (if trend is present) or False (if trend is absence)
     p: float, p value of the significance test
-    z: float, normalized test statistics 
+    z: float, normalized test statistics
     '''
 
     from scipy.stats import norm
-    
+
     n = len(x)
 
     # calculate S
@@ -298,17 +296,17 @@ def _mk_test(x, alpha = 0.05):
     # calculate the var(s)
     if n == g:
         # there is no tie
-        var_s = (n * (n - 1) * (2 * n + 5))/18
+        var_s = (n * (n - 1) * (2 * n + 5)) / 18
     else:
         # there are some ties in data
         tp = np.zeros(unique_x.shape)
         for i in range(len(unique_x)):
             tp[i] = sum(unique_x[i] == x)
-        var_s = (n * (n - 1) * (2 * n + 5) + \
-                np.sum(tp * (tp - 1) * (2 * tp + 5))) / 18
+        var_s = (n * (n - 1) * (2 * n + 5) +
+                 np.sum(tp * (tp - 1) * (2 * tp + 5))) / 18
 
     if s > 0:
-        z = (s - 1)/np.sqrt(var_s)
+        z = (s - 1) / np.sqrt(var_s)
     elif s == 0:
             z = 0
     elif s < 0:
@@ -316,7 +314,7 @@ def _mk_test(x, alpha = 0.05):
 
     # calculate the p_value for two tail test
     p = 2 * (1 - norm.cdf(abs(z)))
-    h = abs(z) > norm.ppf(1 - alpha / 2) 
+    h = abs(z) > norm.ppf(1 - alpha / 2)
 
     if (z < 0) and h:
         trend = 'decreasing'
@@ -346,5 +344,5 @@ def _degradation_CI(results):
 
     sampled_normal = np.random.multivariate_normal(results.params, results.cov_params(), 10000)
     dist = sampled_normal[:, 1] / sampled_normal[:, 0]
-    Rd_CI = np.percentile(dist, [50-34.1, 50+34.1])*100.0
+    Rd_CI = np.percentile(dist, [50 - 34.1, 50 + 34.1]) * 100.0
     return Rd_CI
