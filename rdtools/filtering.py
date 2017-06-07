820e68055c96490396b8999220c4fc416f658040
Dan Ruth <danjruth@gmail.com>
2017-06-07 12:41:49 -0600

    notebook for GA to get detect_clearsky inputs

--- rdtools/filtering.py
+++ rdtools/filtering.py
@@ -153,7 +153,7 @@ def remove_cloudy_times(df,irrad,system_loc,viz=False,correct_bin_labelling=Fals
     
     return df_filtered
     
-def remove_cloudy_days(df,is_clear,start_time='8:00',end_time='16:00',thresh=0.8):
+def remove_cloudy_days(df,is_clear,start_time='7:00',end_time='17:00',thresh=0.8,return_clear_days=False):
     '''
     Filter out entire days that were determined to be cloudy.
     
@@ -193,6 +193,17 @@ def remove_cloudy_days(df,is_clear,start_time='8:00',end_time='16:00',thresh=0.8
     clear_days = pd.Series(index=unique_dates) 
     is_clear_series = pd.Series(index=df.index,data=is_clear) # just the Boolean "is clear", now with an index
     
+    filtered1 = is_clear_series.between_time(start_time,end_time)
+    
+    def enough_points_for_today(s):
+        num_true = len(s[s==True])
+        num_false = len(s[s==False])
+        portion_true = float(num_true)/float(num_false+num_true)        
+        return portion_true>thresh
+        
+    clear_days = filtered1.resample('1D').apply(enough_points_for_today)
+    
+    '''
     # For each date, look at how many times during the day are clear, and call
     # the whole day clear or not.
     for date in unique_dates:
@@ -205,11 +216,15 @@ def remove_cloudy_days(df,is_clear,start_time='8:00',end_time='16:00',thresh=0.8
             clear_days[date] = False
         else:
             clear_days[date] = float(num_true)/float(num_false+num_true) >= thresh
+    '''
 
     # now up-sample this back to the original index
-    clear_days.index = pd.to_datetime(clear_days.index).tz_localize(df.index.tz)
+    #clear_days.index = pd.to_datetime(clear_days.index).tz_localize(df.index.tz)
     clear_times = clear_days.reindex(index=df.index,method='ffill')
     df_filtered = df[clear_times==True]
+    
+    if return_clear_days:
+        return df_filtered,clear_days
     
     return df_filtered
 
@@ -234,6 +249,30 @@ def remove_cloudy_days_from_curve(df,energy,quant=0.90,viz=False,return_when_cle
         returned.    
     '''
     
+    def sweep_slopes(daily_metrics):
+        slopes = np.linspace(0,1,51)
+        ds = slopes[1]-slopes[0]
+        ndiff = pd.Series(index=slopes)
+        for s in slopes:
+            slope_above = s+ds
+            slope = s
+            slope_below = s-ds
+            
+            pred_above = daily_metrics['linelength']*slope_above
+            pred = daily_metrics['linelength']*slope
+            pred_below = daily_metrics['linelength']*slope_below
+            
+            n_above = len(daily_metrics[(daily_metrics['max']>=pred)&(daily_metrics['max']<=pred_above)])
+            n_below = len(daily_metrics[(daily_metrics['max']<=pred)&(daily_metrics['max']>=pred_below)])
+            
+            ndiff.loc[s] = n_below-n_above
+            
+        best_slope = ndiff.idxmax()
+        
+        #clear_days = daily_metrics[(daily_metrics['ratio']>=best_slope-ds)&(daily_metrics['max']<=pred_above)]
+            
+        return best_slope,ndiff
+        
     # df which will store the metrics for each day
     daily_metrics = pd.DataFrame(index = energy.copy().resample('1D').mean().index)
     
@@ -242,17 +281,34 @@ def remove_cloudy_days_from_curve(df,energy,quant=0.90,viz=False,return_when_cle
 
     # function to compute the arc length for a day's energy curve
     def line_length(s):
-        x = abs(s.diff()).sum()
-        return x
-        
+        x = (((s.diff()**2 + 0))**0.5).sum()
+        #x = ((s.diff()**2 + 0).sum())**0.5
+        return x    
+    
+    import matplotlib.pyplot as plt
     # calculate the daily line length, max, and their ratio
     daily_metrics['linelength'] = resampler.apply(line_length)
     daily_metrics['max'] = resampler.max()
     daily_metrics['ratio'] = (daily_metrics['max']/daily_metrics['linelength']).replace([np.inf,-np.inf],np.nan)
+    #daily_metrics = daily_metrics[daily_metrics['max']>=daily_metrics['max'].quantile(0.95)/2]
+                                  
+    best_slope,ndiff = sweep_slopes(daily_metrics)
+    plt.figure()
+    ndiff.plot()
+    plt.title(str(best_slope))
+    best_slope = .5
     
     # select clear days based on the ratio
-    clear_days = daily_metrics['ratio'] > daily_metrics['ratio'].quantile(quant)
+    #clear_days = daily_metrics['ratio'] > daily_metrics['ratio'].quantile(quant)
+    #clear_days = daily_metrics['ratio'] > (daily_metrics['ratio'].min() + quant*(best_slope - daily_metrics['ratio'].min()))
+    tol = 0.03
+    good_above = (daily_metrics['ratio'] > (best_slope*(1-tol)))
+    good_below = (daily_metrics['ratio'] < (best_slope*(1+tol)))
+    good_max = daily_metrics['max'] > 0.4*daily_metrics['max'].quantile(0.95)
     
+    clear_days = good_above & good_below & good_max
+    print(daily_metrics['ratio'])
+        
     # go from clear DAYS to clear TIMES
     clear_inst = clear_days.reindex(index=energy.index,method='ffill')
     
@@ -264,23 +320,43 @@ def remove_cloudy_days_from_curve(df,energy,quant=0.90,viz=False,return_when_cle
         
         import matplotlib.pyplot as plt
         
-        fig = plt.figure(figsize=(9,5))
+        from sklearn.linear_model import LinearRegression
+        print(daily_metrics.loc[clear_days==True,'linelength'].to_frame())
+        model = LinearRegression().fit(daily_metrics.loc[clear_days==True,'linelength'].to_frame(),daily_metrics.loc[clear_days==True,'max'].to_frame())
+        print(model.coef_)
+        print(model.intercept_)
+        x_line = [0.,daily_metrics.loc[clear_days==True,'linelength'].max()]
+        y_line = model.predict(pd.DataFrame(x_line))        
         
-        ax1 = fig.add_subplot(121)
+        fig = plt.figure(figsize=(12,9))
+        
+        ax1 = fig.add_subplot(221)
+        topline = [x*best_slope*(1+tol) for x in x_line]
+        bottomline = [x*best_slope*(1-tol) for x in x_line]
+        ax1.fill_between(x_line,bottomline,topline,facecolor='g',alpha=0.5)
         ax1.scatter(daily_metrics['linelength'],daily_metrics['max'],color='gray',label='cloudy days')
         ax1.scatter(daily_metrics['linelength'][clear_days==True],daily_metrics['max'][clear_days==True],color='g',label='clearsky days')
+        ax1.plot(x_line,y_line,color='r')
         ax1.legend()
         ax1.set_xlabel('daily line length')
         ax1.set_ylabel('daily max')
         
-        ax2 = fig.add_subplot(122)
+        ax2 = fig.add_subplot(222)
         ax2.plot(energy,color='gray')
         ax2.plot(energy[clear_inst==True],'o',color='g')
+        
+        ax3 = fig.add_subplot(223)
+        daily_metrics['ratio'].hist(bins=100,ax=ax3)
+        
+        ax4 = fig.add_subplot(224)
+        daily_metrics['max'].hist(bins=100,ax=ax4)
+        
+        fig.suptitle('Picked '+str(len(clear_days[clear_days==True]))+' clear days out of '+str(len(clear_days)))        
 
         plt.show()
         plt.pause(0.1)
         plt.show()
-        
+                
     if return_when_clear:
         return df_filtered, clear_inst
         
