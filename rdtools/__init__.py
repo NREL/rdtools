6a963cb43ef2986cf15e30ec355008a5df79f392
Michael Deceglie <mdecegli@mdecegli-30388s.local>
2017-07-12 15:37:57 -0600

    Add clearsky tamb from clearsky_temperature branch

--- rdtools/__init__.py
+++ rdtools/__init__.py
@@ -4,6 +4,7 @@ from degradation import degradation_ols
 from degradation import degradation_classical_decomposition
 from degradation import degradation_year_on_year
 from aggregation import aggregation_insol
+from clearsky_temperature import get_clearsky_tamb
 
 from ._version import get_versions
 __version__ = get_versions()['version']
