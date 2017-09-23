493306a470c9d3d2be4a226e20d428cae6a452fb
Michael Deceglie <mdecegli@mdecegli-30388s.local>
2017-09-23 17:45:25 -0600

    PEP8 compliance

--- rdtools/clearsky_temperature.py
+++ rdtools/clearsky_temperature.py
@@ -1,13 +1,9 @@
 import h5py
 from numpy import arange
-from datetime import timedelta, datetime
-import os
+from datetime import timedelta
 import math
 import pandas as pd
 import pkg_resources
-
-
-
 
 
 def get_clearsky_tamb(times, latitude, longitude, window_size=40, gauss_std=20):
@@ -18,15 +14,11 @@ def get_clearsky_tamb(times, latitude, longitude, window_size=40, gauss_std=20):
     :return:            pandas Series of cell sky ambient temperature
     '''
 
-
     filepath = pkg_resources.resource_filename('rdtools', 'data/temperature.hdf5')
 
     buffer = timedelta(days=window_size)
-    interval = times[1] - times[0]
-    freq_actual = pd.infer_freq(times)	
+    freq_actual = pd.infer_freq(times)
     dt_daily = pd.date_range(times[0] - buffer, times[-1] + buffer, freq='D')
-
-
 
     f = h5py.File(filepath, "r")
 
@@ -37,11 +29,10 @@ def get_clearsky_tamb(times, latitude, longitude, window_size=40, gauss_std=20):
     lats = len(a[0, :, 0])
 
     lon_temp = longitude - 180
-    if lon_temp  < 0:
+    if lon_temp < 0:
         lon_temp += 360
     lon_index = round(float(lons) * float(lon_temp) / 360.0)
     lat_index = round(float(lats) * (90.0 - float(latitude)) / 180.0)
-
 
     df = pd.DataFrame(index=dt_daily)
     df['month'] = df.index.month
@@ -52,20 +43,20 @@ def get_clearsky_tamb(times, latitude, longitude, window_size=40, gauss_std=20):
     radius = 0
     for k in range(12):
 
-        day = _get_pixel_value(a,lon_index,lat_index,k,radius)
+        day = _get_pixel_value(a, lon_index, lat_index, k, radius)
         night = _get_pixel_value(b, lon_index, lat_index, k, radius)
 
         if day == float("NaN"):
-            day = a[:,lat_index,k]
+            day = a[:, lat_index, k]
         if night == float("NaN"):
-            night = a[:,lat_index,k]
+            night = a[:, lat_index, k]
 
         ave_day.append(day)
         ave_night.append(night)
 
     for i in range(12):
-        df.loc[df['month']== i+1, 'day'] = ave_day[i]
-        df.loc[df['month'] == i+1, 'night'] = ave_night[i]
+        df.loc[df['month'] == i + 1, 'day'] = ave_day[i]
+        df.loc[df['month'] == i + 1, 'night'] = ave_night[i]
 
     df = df.rolling(window=window_size, win_type='gaussian', min_periods=1, center=True).mean(std=gauss_std)
 
@@ -74,26 +65,26 @@ def get_clearsky_tamb(times, latitude, longitude, window_size=40, gauss_std=20):
 
     df = df.reindex(times, method='nearest')
 
-    #df = df[(df.index >= times[0]) & (df.index <= times[-1])]
+    utc_offsets = [y.utcoffset().total_seconds() / 3600.0 for y in df.index]
 
-    utc_offsets = [y.utcoffset().total_seconds()/3600.0 for y in df.index]
-    solar_noon_offset = lambda utc_offset : longitude / 180.0 * 12.0 - utc_offset
+    def solar_noon_offset(utc_offset):
+        return longitude / 180.0 * 12.0 - utc_offset
     df['solar_noon_offset'] = [solar_noon_offset(utc_offset) for utc_offset in utc_offsets]
 
     df['hour_of_day'] = df.index.hour + df.index.minute / 60.0
     df['Clear Sky Temperature (C)'] = df.apply(lambda x:
-                                               _get_temperature(x['hour_of_day'], x['night'], 
-                                                                x['day'],x['solar_noon_offset']), axis=1)
+                                               _get_temperature(x['hour_of_day'], x['night'],
+                                                                x['day'], x['solar_noon_offset']), axis=1)
     return df['Clear Sky Temperature (C)']
 
 
 def _get_pixel_value(data, i, j, k, radius):
     list = []
-    for x in arange(i-radius,i+radius+1):
+    for x in arange(i - radius, i + radius + 1):
         if x < 0 or x >= len(data[:, 0, 0]):
             continue
         for y in arange(j - radius, j + radius + 1):
-            if y < 0 or y >= len(data[0,:,0]):
+            if y < 0 or y >= len(data[0, :, 0]):
                 continue
 
             value = data[x, y, k]
@@ -116,5 +107,3 @@ def _get_temperature(hour_of_day, night_temp, day_temp, solar_noon_offset):
     v = math.cos((hour_of_day + hour_offset) / 24.0 * 2.0 * math.pi)
     t = t_diff * 0.5 * v * temp_scaler + t_ave
     return t
-
-
