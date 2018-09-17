4e99baf86e0397be0e0b2f5fa99961cc51056c6b
Michael Deceglie <michael.deceglie@nrel.gov>
2018-09-17 16:34:53 -0600

    Test and support python 3.7

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering',
 ]
 
