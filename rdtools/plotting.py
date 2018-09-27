d248a54a91cfe039de36834d3b68cbab0c449323
amnag <ambarishnag@gmail.com>
2018-09-27 12:50:46 -0600

    Added documentation to plotting functions

--- rdtools/plotting.py
+++ rdtools/plotting.py
@@ -1,6 +1,5 @@
 ''' Plotting Module
-
-This module contains plotting functions.
+This module contains plotting functions for common diagonostic plots.
 '''
 
 import matplotlib
@@ -15,6 +14,34 @@ def xyplot(x, y, set_xlimits=False, set_ylimits=False,\
            xmin=None, xmax=None, ymin=None, ymax=None,\
            xlabel='', ylabel='', plot_title='',\
            autoformat_xdate=True, fmt='o', alpha=0.5, figsize=(4.5,3)):
+    '''
+    Description
+    -----------
+    Simple (x,y) scatter plot for Pandas Time Series
+
+    Parameters
+    ----------
+    x: Pandas Time Series (numeric)
+    y: Pandas Time Series (numeric)
+    set_xlimits: whether to set limits on the x-axis (boolean) 
+    set_ylimits: whether to set limits on the y-axis (boolean) 
+    xmin: lower limit of x-axis (numeric)
+    xmax: upper limit of x-axis (numeric)
+    ymin: lower limit of y-axis (numeric)
+    ymax: upper limit of y-axis (numeric)
+    xlabel: label for x-axis (string)
+    ylabel: label for y-axis (string)
+    plot_title: title for plot (string)
+    autoformat_xdate: whether to automaticaly format x-axis datetime labels (boolean)
+    fmt: basic formatting like color, marker and linestyle (string)
+    alpha: transparency, 0.0 transparent through 1.0 opaque (numeric)
+    figsize: width, height in inches (tuple(numeric))
+
+
+    Returns
+    -------
+    Displays a figure
+    '''
 
     fig, ax = plt.subplots(figsize=figsize)
     ax.plot(x, y, fmt, alpha = alpha)
@@ -56,8 +83,44 @@ def xy2plot(x, y1, y2,\
             legend_loc='upper center',legend_bbox_to_anchor=None,\
             legend_shadow = False, legend_ncol=1,\
             label1=None, label2=None):
-    
-    
+    '''
+    Description
+    -----------
+    Simple (x,y1,y2) scatter plot for Pandas Time Series
+
+    Parameters
+    ----------
+    x: Pandas Time Series (numeric)
+    y1: Pandas Time Series (numeric)
+    y2: Pandas Time Series (numeric)
+    set_xlimits: whether to set limits on the x-axis (boolean) 
+    set_ylimits: whether to set limits on the y-axis (boolean) 
+    xmin: lower limit of x-axis (numeric)
+    xmax: upper limit of x-axis (numeric)
+    ymin: lower limit of y-axis (numeric)
+    ymax: upper limit of y-axis (numeric)
+    xlabel: label for x-axis (string)
+    ylabel: label for y-axis (string)
+    autoformat_xdate: whether to automaticaly format x-axis datetime labels (boolean)
+    fmt1: basic formatting like color, marker and linestyle for y1 (string)
+    alpha1: transparency, 0.0 transparent through 1.0 opaque for y1 (numeric)
+    fmt2: basic formatting like color, marker and linestyle for y2 (string)
+    alpha2: transparency, 0.0 transparent through 1.0 opaque for y2 (numeric)
+    figsize: width, height in inches (tuple(numeric))
+    plot_title: title for plot (string)
+    with_legend: whether to create legend (boolean)
+    legend_loc: legend location (string)
+    legend_bbox_to_anchor: parameters for manual legend placement (tuple(numeric))
+    legend_shadow: whether to include legend shadow (boolean)
+    legend_ncol: number of columns in legend (integer)
+    label1: label for y1 (string)
+    label2: label for y2 (string)
+
+
+    Returns
+    -------
+    Displays a figure
+    '''
 
     fig, ax = plt.subplots(figsize=figsize)
     
@@ -100,6 +163,42 @@ def degradation_summary_plots(x, y, yoy_info, yoy_rd, yoy_ci, daily,\
                               set_hist_xlimits=False, set_scatter_ylimits=False,\
                               hist_xmin=-30, hist_xmax=45, scatter_ymin=0.5, scatter_ymax=1.2,\
                               plot_color='blue', summary_title=None):
+    '''
+    Description
+    -----------
+    Function to create plots (scatter plot and histogram) that summarize degradation analysis results
+
+    Parameters
+    ----------
+    x: list of inital and final indices of the aggregated data (timestamp)
+    y: list of initial and final relative performances (numeric)
+    yoy_info: dict
+            ('YoY_values') pandas series of right-labeled year on year slopes
+            ('renormalizing_factor') float of value used to recenter data
+            ('exceedance_level') the degradation rate that was outperformed with
+            a probability given by the exceedance_prob parameter in
+            the degradation_year_on_year function of the degradation module
+    yoy_rd: rate of relative performance change in %/yr (float)
+    yoy_ci: one-sigma confidence interval of degradation rate estimate (float)
+    daily: Pandas Time Series (numeric) 
+         cotaining data that is normalized, filtered and aggregated on the daily scale.
+    set_hist_xlimits: whether to set limits on the x-axis for the histogram (boolean) 
+    set_scatter_ylimits: whether to set limits on the y-axis for the scatter plot (boolean) 
+    hist_xmin: lower limit of x-axis for the histogram (numeric)
+    hist_xmax: upper limit of x-axis for the histogram (numeric)
+    scatter_ymin: lower limit of y-axis for the scatter plot (numeric)
+    scatter_ymax: upper limit of y-axis for the scatter plot (numeric)
+    plot_color: color of the summary plots
+    summary_title: overall title for summary plots (string)
+
+    It should be noted that the yoy_rd, yoy_ci and yoy_info are the outputs from 
+    the degradation_year_on_year function of the degradation module
+
+
+    Returns
+    -------
+    Displays two figures summarizing degradation analysis results
+    '''
 
     yoy_values = yoy_info['YoY_values']
     
