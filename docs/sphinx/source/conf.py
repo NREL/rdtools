083d183ca835edb8de697af5724d99a989a40ec1
Kevin Anderson <kevin.anderson@nrel.gov>
2019-11-07 12:00:24 -0700

    ignore modules.rst because we don't need it and it just generates warnings

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -45,7 +45,9 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+
+# ignore modules.rst since we only have one module
+exclude_patterns = ['modules.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
