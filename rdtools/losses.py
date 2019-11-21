8a3c58a778758ebdfd1327dc2362961896bc02c5
Kevin Anderson <kevin.anderson@nrel.gov>
2019-11-21 13:39:00 -0700

    improvements to is_online and tests

--- rdtools/losses.py
+++ rdtools/losses.py
@@ -5,14 +5,17 @@ import numpy as np
 def is_online(inverters, meter, inverter_limits=None):
     times = inverters.index
     inverters = inverters.fillna(0)
+    meter = meter.fillna(0)
+    n_inv = inverters.shape[1]
 
     if inverter_limits is None:
         inverter_limits = inverters.quantile(0.99)
 
+    # inverter-specific threshold to determine online or offline
     low_limit = 0.01 * inverter_limits
 
-    # estimate the number of inverters online based on the mean reported
-    # inverter power.  this is more robust to cases where some inverters
+    # detect inverter downtime based on the mean reported
+    # inverter power.  this is more robust to cases where inverters
     # are online but not reporting data.  note that it assumes that the
     # mean reporting power is the same as the mean power -- not robust to
     # inverters of vastly different sizes with some not reporting.
@@ -24,21 +27,32 @@ def is_online(inverters, meter, inverter_limits=None):
                                    .divide(relative_sizing, axis=1) \
                                    .mean(axis=1)
 
-    # if no inverters appear online, just use meter / N
-    n_inv = inverters.shape[1]
-    mean_inverter_power[~(inverters > low_limit).any(axis=1)] = meter / n_inv
+    # if no inverters appear online, can't determine mean inverter power
+    all_inverters_appear_offline = (inverters < low_limit).all(axis=1)
+    mean_inverter_power[all_inverters_appear_offline] = meter / n_inv
 
-    # estimate number of inverters online
-    n_inv_online = (meter / mean_inverter_power).round().fillna(0)
-    n_inv_online = n_inv_online.clip(lower=0, upper=n_inv)
+    # if both meter and inverters look offline, we'll say it's offline
+    meter_appears_offline = meter < low_limit.sum()
+    site_offline = all_inverters_appear_offline & meter_appears_offline
+
+    # calculate % diff between theoretical production if all invs were online
+    # and actual meter readings
+    meter_delta = 1 - meter / (n_inv * mean_inverter_power)
+
+    # calculate the expected delta if the smallest inverter is offline:
+    smallest_delta = (relative_sizing / relative_sizing.sum()).min()
+    meter_appears_low = meter_delta > (0.75 * smallest_delta)
+
+    # if meter is low enough relative to inverters that one might be offline,
+    # AND some actually look offline, assume there are offline inverters
+    inverters_appear_offline = ~(inverters > low_limit).all(axis=1)
+    inverters_offline = inverters_appear_offline & meter_appears_low
 
     # assume that if at least 1 inv is offline, any inv < 1% of max is
-    # offline.  this falls down if invs are online but not reporting.
-    # the less than 1% rule is to accomodate the rare case that an inverter
-    # is reporting some nonzero production but is effectively offline.
-    online_mask = pd.DataFrame(index=times, columns=inverters.columns,
+    # offline.  this falls down if some invs are online but not reporting.
+    online_mask = pd.DataFrame(index=times,
+                               columns=inverters.columns,
                                data=True)
-    online_mask.loc[n_inv_online < n_inv, :] = \
-        inverters.gt(0.01 * inverter_limits)
-
+    online_mask.loc[inverters_offline, :] = inverters.gt(low_limit)
+    online_mask.loc[site_offline, :] = False
     return online_mask
--- rdtools/test/losses_test.py
+++ rdtools/test/losses_test.py
@@ -62,37 +62,34 @@ def test_is_online_single_inverter_offline(offline_value):
     assert math.isclose(uptime, 30/31, rel_tol=.01)
 
 
-def test_is_online_multiple_inverters_online():
-    # three inverters, always online during day
-    inv = meter / 3
-    online_invs = pd.DataFrame({
-        0: inv,
-        1: inv,
-        2: inv
-    })
-
-    mask = is_online(online_invs, meter)
-    uptime = mask.loc[is_daylight, :].mean().mean()
-    assert math.isclose(uptime, 1)
-
-
+@pytest.mark.parametrize("relative_sizing", [0.25, 0.5, 1.0, 2.0, 4.0])
 @pytest.mark.parametrize("offline_value", [0, np.nan, 1e-3])
 @pytest.mark.parametrize("offline_invs, expected_uptime", [
-        ([0], 1 - (1/31) / 3),  # individual inverters offline
-        ([1], 1 - (1/31) / 3),
-        ([2], 1 - (1/31) / 3),
+        ([],      1),                   # no downtime
+        ([0],     1 - (1/31) / 3),      # individual inverters offline
+        ([1],     1 - (1/31) / 3),
+        ([2],     1 - (1/31) / 3),
         ([0,1],   1 - (1/31) * 2 / 3),  # first two inverters offline
         ([0,1,2], 1 - (1/31))           # all three inverters offline
 ])
-def test_is_online_multiple_inverters_offline(offline_value,
+def test_is_online_multiple_inverters_offline(relative_sizing,
+                                              offline_value,
                                               offline_invs,
                                               expected_uptime):
+    """
+    Test if losses.is_online can handle:
+        - any number of inverters offline
+        - wide range of relative inverter sizing
+        - various symptoms of being offline (0, nan, 0 < epsilon << 100%)
+        - inverters that look offline but are really just not communicating
+          (doesn't work if some are comms and some are actually offline)
+    """
     # N inverters offline with various readings for one day
-    inv = meter / 3
+    inv = meter / (relative_sizing + 1 + 1/relative_sizing)
     online_invs = pd.DataFrame({
-        0: inv,
+        0: inv * relative_sizing,
         1: inv,
-        2: inv
+        2: inv / relative_sizing
     })
 
     invs = online_invs.copy()
@@ -104,8 +101,9 @@ def test_is_online_multiple_inverters_offline(offline_value,
     assert math.isclose(uptime, 1)
 
     # meter matches inverter sum, aka real downtime
-    meter_adjusted = meter.copy()
-    meter_adjusted *= (1 - len(offline_invs)/3)
+    invs_zero = online_invs.copy()
+    invs_zero.loc['2019-01-10', offline_invs] = 0
+    meter_adjusted = invs_zero.sum(axis=1)
     mask = is_online(invs, meter_adjusted)
     uptime = mask.loc[is_daylight, :].mean().mean()
     assert math.isclose(uptime, expected_uptime, rel_tol=0.01)
