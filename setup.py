d43032409298597c402f3aa58ab7d5e2f81edb00
Kevin Anderson <kevin.anderson@nrel.gov>
2020-01-30 08:55:29 -0700

    Drop 2.7 and add 3.7 and 3.8 to testing, update docs.

--- setup.py
+++ setup.py
@@ -48,10 +48,10 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Intended Audience :: Science/Research',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Topic :: Scientific/Engineering',
 ]
 
