9fcacf8612ffa318ed58d78e79986053067cb29f
Kevin Anderson <57452607+kanderso-nrel@users.noreply.github.com>
2020-03-11 10:41:46 -0600

    Cherry-pick the plotting module out of the system analysis branch (#138)
    * cherry-pick the plotting module commit
    
    * add module-level docstring
    
    * make plotting module docstrings numpydoc-compliant
    
    * update deg/soiling example to use plotting functions
    
    * add plotting module to api.rst
    
    * fix crosslinks
    
    * add matplotlib to setup.py
    
    * update requirements.txt matplotlib for 3.8 testing
    
    * update changelog
    
    * add bins parameter to plotting.degradation_summary_plot from #132
    
    * add basic plotting tests
    
    * exercise all kwargs in plotting tests
    
    * simplify pytest fixture; test kwargs in separate functions
    
    * missed one
    
    * fancy error messages
    
    * Clean up errant spaces
    
    * remove normalize_with_pvwatts import
    
    Co-authored-by: Michael Deceglie <michael.deceglie@nrel.gov>
    Co-authored-by: Michael Deceglie <mdeceglie@users.noreply.github.com>

--- rdtools/__init__.py
+++ rdtools/__init__.py
@@ -13,6 +13,10 @@ from rdtools.filtering import poa_filter
 from rdtools.filtering import tcell_filter
 from rdtools.filtering import clip_filter
 from rdtools.soiling import soiling_srr
+from rdtools.plotting import degradation_summary_plots
+from rdtools.plotting import soiling_monte_carlo_plot
+from rdtools.plotting import soiling_interval_plot
+from rdtools.plotting import soiling_rate_histogram
 
 from ._version import get_versions
 __version__ = get_versions()['version']
