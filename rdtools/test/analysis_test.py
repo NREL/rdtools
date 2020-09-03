b68b80947e74be106ce098ab631fd12fcd3e347c
cdeline <chris.deline@nrel.gov>
2020-09-03 15:31:05 -0600

    clearsky test functional

--- rdtools/test/analysis_test.py
+++ rdtools/test/analysis_test.py
@@ -24,8 +24,8 @@ def test_sensor_analysis():
     rd = analysis.RdAnalysis(power, power*1000, ambient_temperature = power*0+25, temperature_coefficient=meta['tempco'],
                 pvlib_location=loc, pv_tilt=meta['tilt'], pv_azimuth=meta['azimuth'], 
                 interp_freq='D', temperature_model = {'a': -3.47, 'b': -0.0594, 'deltaT': 3}) # temperature_model = "open_rack_glass_glass"
-    # need cell or ambient temp for sensor_analysis
-    rd.sensor_analysis(analyses=['yoy_degradation']) #, 'srr_soiling'
+
+    rd.sensor_analysis(analyses=['yoy_degradation'])
 
     yoy_results = rd.results['sensor']['yoy_degradation']
 
@@ -42,17 +42,22 @@ def test_clearsky_analysis():
         "tilt": 20}
     loc = pvlib.location.Location(meta['latitude'], meta['longitude'], tz = meta['timezone'])
     power = get_energy().tz_localize(meta['timezone'])
+    # initialize the RdAnalysis class
+    temp = analysis.RdAnalysis(power, power*1000, pvlib_location=loc, pv_tilt=meta['tilt'], pv_azimuth=meta['azimuth'] )
+    # get clearsky expected
+    temp.clearsky_preprocess()  # get expected clear-sky values in clearsky_poa.
+    cs = temp.clearsky_poa
     
-    rd = analysis.RdAnalysis(power, power*1000, temperature_coefficient=meta['tempco'],
-                pvlib_location=loc, pv_tilt=meta['tilt'], pv_azimuth=meta['azimuth'], 
-                )
-    # need cell or ambient temp for sensor_analysis
-
+    rd = analysis.RdAnalysis(power*cs, cs, temperature_coefficient=meta['tempco'],
+            pvlib_location=loc, pv_tilt=meta['tilt'], pv_azimuth=meta['azimuth'], 
+            temperature_model = {'a': -3.47, 'b': -0.0594, 'deltaT': 3}
+            )
+    
     rd.clearsky_analysis()
     cs_yoy_results = rd.results['clearsky']['yoy_degradation']
 
-    assert -3.8677 == pytest.approx(cs_yoy_results['p50_rd'], abs=1e-4)
-    assert [-4.7, -3.8] == pytest.approx(cs_yoy_results['rd_confidence_interval'], abs=1e-1)
+    assert -4.744 == pytest.approx(cs_yoy_results['p50_rd'], abs=1e-3)
+    assert [-4.75, -4.73] == pytest.approx(cs_yoy_results['rd_confidence_interval'], abs=1e-2)
 
 
 def test_srr_soiling():  
