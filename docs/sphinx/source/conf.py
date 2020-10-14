f271b9c994753a58027321947b0c9e20ee80ebd2
Kevin Anderson <kevin.anderson@nrel.gov>
2020-10-14 12:34:59 -0600

    first cut at changelog cleanup

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -53,7 +53,8 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 
-exclude_patterns = []
+exclude_patterns = ['changelog/*']
+
 
 source_suffix = ['.rst', '.md']
 
