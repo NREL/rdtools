88c3e114cba3ec23fe34d7d202019ae9dd786765
Kevin Anderson <kevin.anderson@nrel.gov>
2020-11-18 20:03:00 -0700

    first cut

--- setup.py
+++ setup.py
@@ -49,7 +49,8 @@ INSTALL_REQUIRES = [
 EXTRAS_REQUIRE = {
     'doc': [
         'sphinx==1.8.5',
-        'nbsphinx==0.5.0',
+        'nbsphinx==0.6.0',
+        'sphinx-gallery==0.8.1',
         'nbsphinx-link==1.3.0',
         'pandas==0.23.0',
         'pvlib==0.7.1',
