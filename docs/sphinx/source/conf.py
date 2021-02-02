49d88974acb25b4c30ff640a7e1adb43d9d59501
Mark Mikofski <bwana.marko@yahoo.com>
2021-02-02 13:05:07 -0800

    remove trailing single quote in doc confs
    Co-authored-by: Michael Deceglie <mdeceglie@users.noreply.github.com>

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -92,7 +92,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3.8/', None),
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
     'numpy': ('https://numpy.org/doc/stable/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None'),
+    'scipy': ('https://docs.scipy.org/doc/scipy/reference/', None),
 }
 
 
