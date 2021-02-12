76e9a79f0ecda1c1179bcc4e368c497bdc46e0dc
Åsmund Skomedal <Asmund.Skomedal@ife.no>
2021-02-12 07:28:58 +0100

    Update soiling tests

--- rdtools/test/soiling_cods_test.py
+++ rdtools/test/soiling_cods_test.py
@@ -1,12 +1,14 @@
+'''Test methods for the CODS-method to soiling analysis'''
 import pandas as pd
 import numpy as np
-from rdtools.soiling import CODSAnalysis, soiling_cods
+import rdtools.soiling as soiling
 import pytest
 
 
 def test_iterative_signal_decomposition(cods_normalized_daily):
+    ''' Test iterative_signal_decomposition with fixed test case '''
     np.random.seed(1977)
-    cods = CODSAnalysis(cods_normalized_daily)
+    cods = soiling.CODSAnalysis(cods_normalized_daily)
     df_out, degradation, soiling_loss, rs, RMSE, sss, adf_res = \
         cods.iterative_signal_decomposition()
     assert 0.080563 == pytest.approx(degradation, abs=1e-6),\
@@ -45,17 +47,59 @@ def test_iterative_signal_decomposition(cods_normalized_daily):
         ['soiling_ratio', 'soiling_rates', 'cleaning_events',
          'seasonal_component', 'degradation_trend', 'total_model', 'residuals']]
     pd.testing.assert_series_equal(expected_means, df_out.mean(),
-                                   check_exact=False, check_less_precise=6)
+                                   check_exact=False, check_less_precise=True)
+
+
+def test_iterative_signal_decompositionwith_nan_interval(cods_normalized_daily):
+    ''' Test the CODS algorithm with fixed test case with a NaN period'''
+    normalized_corrupt = cods_normalized_daily.copy()
+    normalized_corrupt[26:50] = np.nan
+    np.random.seed(1977)
+    cods = soiling.CODSAnalysis(normalized_corrupt)
+    df_out, degradation, soiling_loss, rs, RMSE, sss, adf_res = \
+        cods.iterative_signal_decomposition()
+    assert -0.004968 == pytest.approx(degradation, abs=1e-5),\
+        'Degradation rate different from expected value'
+    assert 3.232171 == pytest.approx(soiling_loss, abs=1e-5),\
+        'Soiling loss different from expected value'
+    assert 1.000108 == pytest.approx(rs, abs=1e-5),\
+        'Residual shift different from expected value'
+    assert 0.008184 == pytest.approx(RMSE, abs=1e-5),\
+        'RMSE different from expected value'
+    assert not sss, \
+        'Small soiling signal assertion different from expected value'
+    assert 1.230754e-8 == pytest.approx(adf_res[1], abs=1e-6),\
+        'p-value of Augmented Dickey-Fuller test different from expected value'
+
+    # Check result dataframe
+    assert isinstance(df_out, pd.DataFrame), 'result_df not a dataframe'
+    expected_means = pd.Series({'soiling_ratio': 0.967678,
+                                'soiling_rates': -0.002366,
+                                'cleaning_events': 0.045082,
+                                'seasonal_component': 1.000192,
+                                'degradation_trend': 0.999950,
+                                'total_model': 0.967915,
+                                'residuals': 1.000108})
+    expected_means = expected_means[
+        ['soiling_ratio', 'soiling_rates', 'cleaning_events',
+         'seasonal_component', 'degradation_trend', 'total_model', 'residuals']]
+    pd.testing.assert_series_equal(expected_means, df_out.mean(),
+                                   check_exact=False, check_less_precise=True)
 
 
 def test_soiling_cods(cods_normalized_daily):
+    ''' Test the CODS algorithm with fixed test case and 16 repetitions'''
     reps = 16
     np.random.seed(1977)
-    sr, sr_ci, deg, deg_ci, result_df = soiling_cods(cods_normalized_daily, reps=reps)
+    sr, sr_ci, deg, deg_ci, result_df = soiling.soiling_cods(cods_normalized_daily, reps=reps)
     assert 0.962207 == pytest.approx(sr, abs=1e-1),\
         'Soiling ratio different from expected value'
     assert np.array([0.96662419, 0.95692131]) == pytest.approx(sr_ci, abs=1e-1),\
         'Confidence interval of SR different from expected value'
+    assert 0.09 == pytest.approx(deg, abs=0.2),\
+        'Degradation rate different from expected value'
+    assert np.array([-0.17143952,  0.39313724]) == pytest.approx(deg_ci, abs=0.2),\
+        'Confidence interval of degradation rate different from expected value'
 
     # Check result dataframe
     expected_summary_columns = \
@@ -73,14 +117,22 @@ def test_soiling_cods(cods_normalized_daily):
             "'{}' was expected as a column, but not in result_df".format(x)
 
 
-def test_soiling_cods_with_nan_interval(cods_normalized_daily):
-    reps = 16
-    normalized_corrupt = cods_normalized_daily.copy()
-    normalized_corrupt[26:50] = np.nan
-    np.random.seed(1977)
-    sr, sr_ci, deg, deg_ci, result_df = soiling_cods(normalized_corrupt, reps=reps)
-    assert 0.9626787 == pytest.approx(sr, abs=1e-1),\
-        'Soiling ratio different from expected value when an entire interval was NaN'
-    assert np.array([0.96626472, 0.95986474]) == pytest.approx(sr_ci, abs=1e-1),\
-        'Confidence interval of SR different from expected value when an '\
-        + 'entire interval was NaN'
+def test_Kalman_filter_for_SR(cods_normalized_daily):
+    '''Test the Kalman Filter method in CODS'''
+    cods = soiling.CODSAnalysis(cods_normalized_daily)
+    dfk, Ps = cods._Kalman_filter_for_SR(cods_normalized_daily)
+
+    # Check if results are okay
+    assert dfk.isna().sum().sum() == 0, "NaNs were found in Kalman Filter results"
+    assert (dfk.index == cods_normalized_daily.index).all(), \
+        "Index returned from Kalman Filter is not as expected"
+    expected_columns = ['raw_pi', 'raw_rates', 'smooth_pi', 'smooth_rates', 'soiling_ratio',
+                        'soiling_rates', 'cleaning_events', 'days_since_ce']
+    actual_columns = dfk.columns.values
+    for x in actual_columns:
+        assert x in expected_columns,\
+            "'{}' not an expected column in Kalman Filter results]".format(x)
+    for x in expected_columns:
+        assert x in actual_columns,\
+            "'{}' was expected as a column, but not in Kalman Filter results".format(x)
+    assert Ps.shape == (732, 2, 2), "Shape of array of covariance matrices (Ps) not as expected"
