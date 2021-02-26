56586a8a15212fe0886b7c2b9bfdc8f4677750b7
kperry2215 <kirsten.perry2215@gmail.com>
2021-02-26 16:24:48 -0700

    Refactoring of logic-based method (vectorized looping).

--- rdtools/filtering.py
+++ rdtools/filtering.py
@@ -188,8 +188,7 @@ def logic_clip_filter(power_ac,
     #Make copies of the original inputs for the cases that the data is changes for clipping evaluation
     original_time_series_sampling_frequency = time_series_sampling_frequency
     power_copy = power_ac.copy()
-    #duplicate indexes are dropped and the a frequency is given to the index
-    #testing shows that pandas.rolling(periods) performs consistently only when the datetime index has a set frequency
+    #Drop duplciate indices
     power_ac = power_ac.reset_index().drop_duplicates(subset = power_ac.index.name, 
                                                       keep = 'first').set_index(power_ac.index.name)
     freq_string = str(time_series_sampling_frequency) + 'T'
@@ -209,10 +208,10 @@ def logic_clip_filter(power_ac,
         roll_periods = 5
     else:
         roll_periods = 3
-    #Replace the lower 10% of daily data with NaN's
-    daily = 0.1 * power_ac.resample('D').max()
-    power_ac['tenP_daily'] = daily.reindex(index = power_ac.index, method='ffill')
-    power_ac.loc[power_ac[column_name] < power_ac['tenP_daily'], column_name]= np.nan   
+    #Replace the lower 25% of daily data with NaN's
+    daily = 0.25 * power_ac.resample('D').max()
+    power_ac['ten_percent_daily'] = daily.reindex(index = power_ac.index, method='ffill')
+    power_ac.loc[power_ac[column_name] < power_ac['ten_percent_daily'], column_name]= np.nan   
     power_ac = power_ac[column_name]
     
     #Calculate the maximum value over a forward-rolling window
@@ -229,23 +228,31 @@ def logic_clip_filter(power_ac,
     #the following applies the clipping determination to all data points within
     #the rolling window
     #Get max derivative at a certain timestamp, looks at the periods     
-    clipping = roll_clip_mask.copy()
-    if roll_periods > 1:
-        i=0
-        for c in roll_clip_mask:
-            if c==True: 
-                clipping[i+1] = True
-                clipping[i+2] = True
-                if roll_periods > 3:
-                    clipping[i + 3] = True
-                if roll_periods > 4:    
-                    clipping[i + 4] = True
-            i=i+1
-    # high frequency was resampled to 15 minute average data
-    #the following lines apply the 15 minute clipping filter to the
-    #original 15 minute data resulting in a clipping filter on the orginal
+    clipping_df = pd.DataFrame(roll_clip_mask.copy())
+    #Set values within roll_periods values from a True instance
+    #as True as well
+    clipping_df['subgroup'] = (clipping_df['value'] != clipping_df['value'].shift(1)).cumsum()
+    clipping_df['subgroup_count'] = clipping_df.groupby(clipping_df['subgroup']).cumcount() + 1
+    if roll_periods > 4:
+        clipping_df.loc[(clipping_df['value'] == False) &
+                        (clipping_df['subgroup_count'] <= 4) &
+                        (clipping_df['subgroup'] > 1), 'value'] = True  
+    elif roll_periods > 3:
+        clipping_df.loc[(clipping_df['value'] == False) &
+                        (clipping_df['subgroup_count'] <= 3) &
+                        (clipping_df['subgroup'] > 1), 'value'] = True
+    elif roll_periods > 1:
+        clipping_df.loc[(clipping_df['value'] == False) &
+                        (clipping_df['subgroup_count'] <= 2) &
+                        (clipping_df['subgroup'] > 1), 'value'] = True
+    else:
+        pass
+    clipping = clipping_df['value']
+    #High frequency was resampled to 15-minute average data.
+    #The following lines apply the 15-minute clipping filter to the
+    #original 15-minute data resulting in a clipping filter on the original
     #data   
-    if original_time_series_sampling_frequency < 10:
+    if (original_time_series_sampling_frequency < 10):
         power_ac = power_copy.copy()
         clipping = clipping.reindex(index=power_ac.index,
                                     method='ffill')
@@ -264,18 +271,16 @@ def logic_clip_filter(power_ac,
     else:  
         #find the maximum and minimum power level where clipping is found each day
         clip_pwr = power_ac[clipping]
-        clip_pwr = clip_pwr.reindex(index=power_ac.index,fill_value=np.nan)
+        clip_pwr = clip_pwr.reindex(index=power_ac.index,
+                                    fill_value=np.nan)
         daily_clipping_max = clip_pwr.resample('D').max()
         daily_clipping_min = clip_pwr.resample('D').min()
-        daily_clipping_min = daily_clipping_min.reindex(index=power_ac.index,method='ffill')
-        daily_clipping_max = daily_clipping_max.reindex(index=power_ac.index,method='ffill')
+        daily_clipping_min = daily_clipping_min.reindex(index=power_ac.index,
+                                                        method='ffill')
+        daily_clipping_max = daily_clipping_max.reindex(index=power_ac.index,
+                                                        method='ffill')
     #set all values to clipping that are between the maximum and minimum power levels
     #where clipping was found on a daily basis
-    #note that this bins data as clipping that is not necessary clipped
-    #for example, for one day, daily data on a 50 kW is found to be clipped at 49kW
-    # and 30 kW.  This is atypical for PV systems but such a finding brings concern
-    #to what is  happening on the said.   It was thought to be conservative to label
-    #all data between 30 and 49kW as clipping so as to not use this data in further analysis
     final_clip=(daily_clipping_min <= power_ac) & (power_ac <= daily_clipping_max)
     final_clip=final_clip.reindex(index = power_copy.index,
                                   fill_value = False)
@@ -305,6 +310,14 @@ def xgboost_clip_filter(power_ac,
         True values delineate clipping periods, and False values delineate non-
         clipping periods.
     """
+    #Check that it's a Pandas series with a datetime index. If not, raise an error.
+    if not isinstance(power_ac.index, pd.DatetimeIndex):
+        raise TypeError('Must be a Pandas series with a datetime index.')
+    #Check the other input variables to ensure that they are the correct format
+    if (mounting_type != "Tracking") & (mounting_type != "Fixed"):
+        raise ValueError("Variable mounting_type must be string 'Tracking' or 'Fixed'.")
+    #Convert the Pandas series to a dataframe, with mounting_type as an 
+    #additional column.
     power_ac_df = power_ac.to_frame()
     power_ac_df['mounting_config'] = mounting_type
     #Get the sampling frequency (as a continuous feature variable)
@@ -328,7 +341,6 @@ def xgboost_clip_filter(power_ac,
     #First order derivative for the rolling average
     power_ac_df['first_order_derivative_backward_rolling_avg'] = power_ac_df.rolling_average.diff()
     power_ac_df['first_order_derivative_forward_rolling_avg'] = power_ac_df.rolling_average.shift(-1).diff()    
-    
     #Rolling max derivative over time
     max_roll = power_ac_df['scaled_value'].iloc[::-1].rolling(rolling_window).max()
     max_roll = max_roll.reindex(power_ac_df.index)
