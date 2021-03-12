7db673d532c76d3109c47d825ec8839f9cd60996
kperry2215 <kirsten.perry2215@gmail.com>
2021-03-11 17:28:01 -0700

    Fixing the file path.

--- rdtools/filtering.py
+++ rdtools/filtering.py
@@ -8,12 +8,12 @@ import os
 
 # Load in the XGBoost clipping model using joblib.
 xgboost_clipping_model = None
-model_path = os.path.dirname(os.getcwd()).replace("\\", "/") + \
-                        "/models/xgboost_clipping_model.dat"
 
 
 def _load_xgboost_clipping_model():
     global xgboost_clipping_model
+    model_path = os.path.dirname(os.getcwd()).replace("\\", "/") + \
+        "/models/xgboost_clipping_model.dat"
     if xgboost_clipping_model is None:
         xgboost_clipping_model = joblib.load(model_path)
     return xgboost_clipping_model
