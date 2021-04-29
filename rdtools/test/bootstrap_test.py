62c6ac4daff0ca5a2c9273dbbd8979b0459c59a1
Åsmund Skomedal <Asmund.Skomedal@ife.no>
2021-04-29 11:36:04 +0200

    Accomodate name changes

--- rdtools/test/bootstrap_test.py
+++ rdtools/test/bootstrap_test.py
@@ -1,14 +1,14 @@
 '''Bootstrap module tests.'''
 
-from rdtools.bootstrap import construct_confidence_intervals,\
-    make_time_series_bootstrap_samples
+from rdtools.bootstrap import _construct_confidence_intervals,\
+    _make_time_series_bootstrap_samples
 from rdtools.degradation import degradation_year_on_year
 
 
 def test_bootstrap_module(cods_normalized_daily, cods_normalized_daily_wo_noise):
     ''' Test make time serie bootstrap samples and construct of confidence intervals. '''
     # Test make bootstrap samples
-    bootstrap_samples = make_time_series_bootstrap_samples(cods_normalized_daily,
+    bootstrap_samples = _make_time_series_bootstrap_samples(cods_normalized_daily,
                                                            cods_normalized_daily_wo_noise,
                                                            sample_nr=10,
                                                            block_length=90,
@@ -19,8 +19,8 @@ def test_bootstrap_module(cods_normalized_daily, cods_normalized_daily_wo_noise)
     assert bootstrap_samples.shape[1] == 10, "Number of columns in bootstrapped signals is wrong"
 
     # Test construction of confidence intervals
-    confidence_intervals, exceedance_level, metrics = construct_confidence_intervals(
-        bootstrap_samples, degradation_year_on_year, full_calc=False)
+    confidence_intervals, exceedance_level, metrics = _construct_confidence_intervals(
+        bootstrap_samples, degradation_year_on_year, uncertainty_method='none')
 
     # Check if results are as expected
     assert len(confidence_intervals) == 2, "2 confidence interval bounds not returned"
