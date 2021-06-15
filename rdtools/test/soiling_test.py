1720f13a54496b048266eaa3100a19cd55fa1693
Kevin Anderson <kevin.anderson@nrel.gov>
2021-06-15 11:20:31 -0600

    add test

--- rdtools/test/soiling_test.py
+++ rdtools/test/soiling_test.py
@@ -259,6 +259,34 @@ def test_soiling_srr_min_interval_length_default(soiling_normalized_daily, soili
     assert expected_sr == pytest.approx(sr, abs=1e-6),\
         'Soiling ratio different from expected value'
 
+
+@pytest.mark.parametrize('test_param', ['energy_normalized_daily',
+                                        'insolation_daily',
+                                        'precipitation_daily'])
+def test_soiling_srr_non_daily_inputs(test_param):
+    '''
+    Validate the frequency check for input time series
+    '''
+    dummy_daily_explicit = pd.Series(0, index=pd.date_range('2019-01-01', periods=10, freq='d'))
+    dummy_daily_implicit = pd.Series(0, index=pd.date_range('2019-01-01', periods=10, freq='d'))
+    dummy_daily_implicit.index.freq = None
+    dummy_nondaily = pd.Series(0, index=dummy_daily_explicit.index[::2])
+
+    kwargs = {
+        'energy_normalized_daily': dummy_daily_explicit,
+        'insolation_daily': dummy_daily_explicit,
+        'precipitation_daily': dummy_daily_explicit,
+    }
+    # no error for implicit daily inputs
+    kwargs[test_param] = dummy_daily_implicit
+    _ = SRRAnalysis(**kwargs)
+
+    # yes error for non-daily inputs
+    kwargs[test_param] = dummy_nondaily
+    with pytest.raises(ValueError, match='must have daily frequency'):
+        _ = SRRAnalysis(**kwargs)
+
+
 # ###########################
 # annual_soiling_ratios tests
 # ###########################
