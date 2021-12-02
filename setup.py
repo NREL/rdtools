5326e334b9bd06e6dc55cb8c90908a58871e40ba
Perry <kperry@nrel.gov>
2021-12-02 11:42:02 -0700

    update versioning.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ INSTALL_REQUIRES = [
     'h5py >= 2.7.1',
     'plotly>=4.0.0',
     'joblib >= 0.16.0',
-    'xgboost >= 1.3.3',
+    'xgboost >= 1.5.1',
     'pvlib >= 0.7.0, <0.10.0',
     'scikit-learn >= 0.22.0',
 ]
