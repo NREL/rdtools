050685892f9050f029c42923218cb4790609f703
Kevin Anderson <kevin.anderson@nrel.gov>
2022-09-02 10:03:44 -0400

    experimental environment validation

---
+++ .github/workflows/validate_requirements_versus_installed.py
@@ -0,0 +1,39 @@
+
+import subprocess
+import pkg_resources
+import json
+
+
+def parse_requirements_file(fn):
+    with open(fn, 'r') as f:
+        requirements = pkg_resources.parse_requirements(f.read())
+    packages = [
+        {'name': req.name, 'version': str(req.specifier)[2:]}
+        for req in requirements
+    ]
+    return packages
+
+
+def assert_equal(installed, required):
+    # for now only check package names, not versions
+    installed = {req['name'] for req in installed}
+    required = {req['name'] for req in required}
+    missing = installed - required
+    extra = required - installed
+    if missing or extra:
+        msg = (
+            'Requirements files do not match installed versions:\n'
+            f'Missing dependencies: {missing}\n'
+            f'Extra dependencies: {extra}'
+        )
+        raise AssertionError(msg)
+
+
+out = subprocess.check_output(['pip', 'list', '--format', 'json'])
+installed_packages = json.loads(out.decode())
+
+requirements1 = parse_requirements_file(r'C:\Users\KANDERSO\projects\rdtools\requirements.txt')
+requirements2 = parse_requirements_file(r'C:\Users\KANDERSO\projects\rdtools\docs\notebook_requirements.txt')
+requirements = requirements1 + requirements2
+
+assert_equal(installed_packages, requirements)
