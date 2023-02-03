29fcde3eb268676ae4dbb390c52f786c67e7769b
cdeline <chris.deline@nrel.gov>
2023-02-03 15:06:52 -0700

    add NaN to hampel tests

--- rdtools/test/filtering_test.py
+++ rdtools/test/filtering_test.py
@@ -337,6 +337,7 @@ def test_normalized_filter_default():
 
 
 def test_hampel_filter():
-    a = pd.Series([5, 4, 7, 3, 4, 5, 4, 100, 3, 4, 5, 3, 5, 3, 5, 4, 12])
+    a = pd.Series([np.NaN, 4, 7, 3, 4, 5, 4, 100, 3, 4, 5, 3, 5, 3, 5, 4, 12])
     assert ~(hampel_filter(a).iloc[-1])
     assert ~(hampel_filter(a).iloc[7])
+    assert ~(hampel_filter(a).iloc[0])
