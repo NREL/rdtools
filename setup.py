68ca65bc921d9d1db78b1460f65bbe5f0ddeb753
Kevin Anderson <kevin.anderso@gmail.com>
2023-05-19 14:57:38 -0400

    increase minimum pandas and numpy versions

--- setup.py
+++ setup.py
@@ -41,9 +41,8 @@ TESTS_REQUIRE = [
 
 INSTALL_REQUIRES = [
     'matplotlib >= 3.0.0',
-    'numpy >= 1.15',
-    # exclude pandas==1.0.0 & 1.0.1 for GH142, and 0.24.0 for GH114
-    'pandas >= 0.23.2,!=0.24.0,!=1.0.0,!=1.0.1',
+    'numpy >= 1.17.3',
+    'pandas >= 1.3.0',
     'statsmodels >= 0.9.0',
     'scipy >= 1.1.0',
     'h5py >= 2.8.0',
