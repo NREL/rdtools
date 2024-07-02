197a203a5d2a4caa882b2830103ff0f217f243e5
Michael Deceglie <Michael.Deceglie@nrel.gov>
2024-07-02 12:59:09 -0600

    remove blank line

--- rdtools/analysis_chains.py
+++ rdtools/analysis_chains.py
@@ -539,7 +539,6 @@ class TrendAnalysis:
                 "sensor_clearsky_filter"
             )
 
-
         # note: the previous implementation using the & operator treated NaN
         # filter values as False, so we do the same here for consistency:
         filter_components = pd.DataFrame(filter_components).fillna(False)
