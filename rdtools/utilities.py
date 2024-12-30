2338d92778e6e4b6f09c73fa90caa5fba03a65ed
Martin Springer <97482055+martin-springer@users.noreply.github.com>
2024-12-30 15:08:58 -0500

    update docstring robust mean
    Co-authored-by: Michael Deceglie <mdeceglie@users.noreply.github.com>

--- rdtools/utilities.py
+++ rdtools/utilities.py
@@ -58,7 +58,7 @@ def robust_mean(x, q=0.99):
     Compute the mean of a time series (x), ignoring small values and NaN's.
     NaN's and small values [x < Q(x,q)/1000] are removed before calculating the mean.
     This function ensures that time series with NaN's and distributions without
-    NaN's return the same results.
+    NaN's return the same results.  Should only be used if x is expected to be ≥0.
 
     Parameters
     ----------
